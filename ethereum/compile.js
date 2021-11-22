4379bca07b6c50571998c68070493c63a2e2ad34
James Lincoln <jamesknewy@gmail.com>
2021-11-22 10:48:31 +0000

    commenting compile script

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -11,6 +11,7 @@ const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
 
+// for separate contract objects into json
 for (let contract in output) {
 	fs.outputJsonSync(
 		path.resolve(buildPath, contract.replace(/:/g, "") + ".json"),
